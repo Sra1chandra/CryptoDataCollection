0fdbc4e64d359c4a6556509a6bc71f94176b03fd
Sra1chandra <jssschandra@gmail.com>
2022-01-09 19:33:59 +0530

    Store Data in HDF5 database

---
+++ database.py
@@ -0,0 +1,32 @@
+import h5py
+import numpy as np
+import pandas as pd
+
+class HDF5Client:
+    def __init__(self, exchange):
+        self.hf = h5py.File(f"data/{exchange}.h5", "a")
+        self.hf.flush()
+
+    def create_dataset(self, symbol):
+        if symbol not in self.hf.keys():
+            self.hf.create_dataset(symbol, (0, 6), maxshape=(None, 6), dtype="float64")
+            self.hf.flush()
+
+    def write_data(self, symbol, data):
+        np_data = np.array(data)
+        if symbol not in self.hf.keys():
+            self.create_dataset(symbol)
+
+        self.hf[symbol].resize(self.hf[symbol].shape[0]+np_data.shape[0], axis=0)
+        self.hf[symbol][-np_data.shape[0]:] = np_data
+        self.hf.flush()
+
+    def read_data(self, symbol):
+        hdf5_data = self.hf[symbol]
+        np_data = np.array(hdf5_data)
+
+        df = pd.DataFrame(np_data, columns=["timestamp", "open", "high", "low", "close", "volume"])
+        df["timestamp"] = pd.to_datetime(df["timestamp"].values.astype(np.int64), unit="ms")
+        return df
+
+
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from utils.customLogger import logger
-
 from exchanges.binance import BinanceAPI
+
+from database import HDF5Client
+
 
 if __name__ == "__main__":
     client = BinanceAPI()
-    data = client.get_data('BTCUSDT', '1m')
-    print(len(data))
+    # raw_data = client.get_data('BTCUSDT', '1m')
+    h5_db = HDF5Client("binance")
+    h5_db.create_dataset("BTCUSDT")
+    # data = []
+    # if raw_data is not None:
+    #     for row in raw_data:
+    #         data.append((float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5])))
+    #
+    # h5_db.write_data("BTCUSDT", data)
+
+    print(h5_db.read_data("BTCUSDT"))
+
+
